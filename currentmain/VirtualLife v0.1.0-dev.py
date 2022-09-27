15ffd96f0804e05bbb90ac85051b8f181f0d4aec
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2022-09-27 15:58:14 +0100

    Add files via upload

--- currentmain/VirtualLife v0.1.0-dev.py
+++ currentmain/VirtualLife v0.1.0-dev.py
@@ -8,5 +8,8 @@ if cmdinput == 'help':
     print(div)
     print("COMMANDS as of v0.1.0")
     print(div)
-    
+    print("info | shows current virtuallife information")
+    print("sessioninfo | current information regarding the session")
+    print("onlinebanking | enter the online banking module")
+    print("shop | go shopping")
     
