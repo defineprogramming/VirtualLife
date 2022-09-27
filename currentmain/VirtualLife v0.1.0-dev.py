a2e0e9ef1d3ac1d809c3b1c49fe4ba5253d66bdb
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2022-09-27 13:20:02 +0100

    Add files via upload

---
+++ currentmain/VirtualLife v0.1.0-dev.py
@@ -0,0 +1,12 @@
+import time
+div = "==============="
+print("=============== VirtualLife STARTED ===============")
+time.sleep(1)
+print("Enter a command, or use 'help' for a list of available commands...")
+cmdinput = str(input("Enter command: "))
+if cmdinput == 'help':
+    print(div)
+    print("COMMANDS as of v0.1.0")
+    print(div)
+    
+    
